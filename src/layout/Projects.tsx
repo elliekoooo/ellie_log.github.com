29c2dc073bb19c05522cfc223f4c156ab32681c4
ellie <elliekoooo@gmail.com>
2024-12-11 11:21:07 +0900

    프로필 이미지 바꾸기

--- src/layout/Projects.tsx
+++ src/layout/Projects.tsx
@@ -1,9 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { ImageSlider } from "component/slider";
 import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCar, faDolly, faProjectDiagram } from "@fortawesome/free-solid-svg-icons";
-import { faTypo3 } from "@fortawesome/free-brands-svg-icons";
 
 const Projects:React.FC = (lang: any) => {
     const { t } = useTranslation(lang);
